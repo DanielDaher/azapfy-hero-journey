0f94413cb9fd7b6d5a371fdbd0ec189a1be20f7c
Daniel <danielfdoficial@gmail.com>
2023-10-30 17:01:37 -0300

    style: apply material ui on root project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// https://medium.com/frontendweb/how-to-use-material-ui-with-nextjs-and-react-18-6c054ceacf77
+const clientSideEmotionCache = createEmotionCache();
+
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
--- pages/outrapage.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <p>teste</p>
-    </main>
-  )
-}
