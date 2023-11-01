6516afa8248f4c46e92d08251267e2de70cc635b
Daniel <danielfdoficial@gmail.com>
2023-11-01 20:56:03 -0300

    feat: filter characters on input

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -4,20 +4,33 @@ import HeroCard from '@/components/HeroCard';
 import styles from '@/styles/Home.module.css';
 import { HeroesContext } from "@/context/HeroesContext";
 import { useEffect, useState, useContext } from "react";
+import SearchInput from './SearchInput';
 
 export default function Dashboard() {
   const { heroesData, selectedHeroes, fetchData } = useContext(HeroesContext);
+  const [ currentHeroes, setCurrentHeroes ] = useState([]);
 
   useEffect(() => {
     if (!heroesData.length) {
       fetchData();
     }
-  }, []);
+    if (heroesData.length) {
+      setCurrentHeroes([ ...heroesData ]);
+    }
+  }, [heroesData]);
+
+  const searchHero = (inputValue) => {
+    if (inputValue === '') {
+      return setCurrentHeroes([ ...heroesData ]);
+    }
+    const filteredHeroes = heroesData.filter(({ name }) => name.toUpperCase().includes(inputValue));
+    setCurrentHeroes(filteredHeroes);
+  }
 
   const RenderHeroCards = () => {
     return (
       <div className={styles.cardBoard}>
-        { heroesData.map(( hero ) => <HeroCard key={ hero.id } heroData={ hero } />) }
+        { currentHeroes.map(( hero ) => <HeroCard key={ hero.id } heroData={ hero } />) }
       </div>
       
     )
@@ -25,9 +38,9 @@ export default function Dashboard() {
 
   return (
     <div>
+      {  heroesData.length ? <SearchInput searchHero={ searchHero } /> : '' }
       { heroesData.length ? <RenderHeroCards /> : <Loading /> }
       { selectedHeroes.length === 2 ? <BattleModal /> : '' }
     </div>
   );
-
 }
---
+++ components/SearchInput.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const formStyle = {
+  position: 'fixed',
+  top: '0',
+  width: '100vw',
+  zIndex: 1,
+  backgroundColor: '#121212',
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '1.2em',
+};
+
+const inputMargins = {
+  marginTop: '30px',
+  marginBottom: '10px'
+}
+
+
+export default function SearchInput(props) {
+  const { searchHero } = props;
+  const [ inputValue, setInputValue ] = useState('');
+
+  const cleanInput = () => {
+    setInputValue('');
+    searchHero('');
+  }
+
+  const submitSearch = (e) => {
+    e.preventDefault();
+    searchHero(inputValue);
+  }
+
+  return (
+    <form onSubmit={(e) => submitSearch(e)} style={formStyle}>
+      <TextField
+          label="Pesquisar"
+          variant="outlined"
+          value={inputValue}
+          sx={inputMargins}
+          onChange={(e) => setInputValue(e.target.value.toUpperCase())}
+        />
+        <Button
+          variant="outlined"
+          size="medium"
+          sx={inputMargins}
+          onClick={() => searchHero(inputValue)}
+        >
+          Pesquisar
+        </Button>
+        <Button
+          variant="outlined"
+          size="medium"
+          sx={inputMargins}
+          onClick={() => cleanInput()}
+        >
+          Limpar
+        </Button>
+    </form>
+  );
+}
